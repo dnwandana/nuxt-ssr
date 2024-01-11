efa36a0c3951e83721aaa545b746864cfbb44ab6
Dana Wandana <dn.wandana@gmail.com>
2024-01-11 17:17:44 +0700

    initial commit

---
+++ components/ProductCard.vue
@@ -0,0 +1,57 @@
+<script setup>
+defineProps({
+  id: {
+    type: Number,
+    required: true,
+  },
+  image: {
+    type: String,
+    required: true,
+  },
+  title: {
+    type: String,
+    required: true,
+  },
+  description: {
+    type: String,
+    required: true,
+  },
+});
+</script>
+
+<template>
+  <a-card hoverable>
+    <template #cover>
+      <client-only>
+        <a-image
+          :alt="title"
+          :src="image"
+          height="250px"
+          width="100%"
+          style="object-fit: cover"
+        />
+      </client-only>
+    </template>
+    <a-card-meta>
+      <template #title>
+        <nuxt-link :to="`/product/${id}`">
+          <a-typography-text>
+            {{ title }}
+          </a-typography-text>
+        </nuxt-link>
+      </template>
+      <template #description>
+        <client-only>
+          <a-typography-paragraph
+            type="secondary"
+            :ellipsis="{
+              rows: 3,
+              expandable: false,
+            }"
+            :content="description"
+          />
+        </client-only>
+      </template>
+    </a-card-meta>
+  </a-card>
+</template>
---
+++ pages/index.vue
@@ -0,0 +1,30 @@
+<script setup>
+const { data: products } = await useFetch("https://fakestoreapi.com/products");
+</script>
+
+<template>
+  <main style="min-height: 100vh">
+    <a-row :gutter="16">
+      <a-col
+        :span="6"
+        style="padding: 16px"
+        v-for="product in products"
+        :key="product.id"
+      >
+        <product-card
+          :id="product.id"
+          :image="product.image"
+          :title="product.title"
+          :description="product.description"
+        />
+      </a-col>
+    </a-row>
+  </main>
+</template>
+
+<style>
+body {
+  margin: 0;
+  padding: 0;
+}
+</style>
